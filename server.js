8e8fc4169b596d08aee3456c1b9e599a6715c762
Jonathan Lee <jonathan.lee.devel@gmail.com>
2019-10-04 20:15:08 +0100

    Added HTTP redirect server

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const app = require('express')();
+const httpApp = require('express')();
 const https = require('https');
 const fs = require('fs');
+
+// HTTP Redirect Server
+const HTTP_PORT = 3000;
+const http = require('http');
+httpApp.get('*', (req, res) => {
+	res.redirect('https://' + req.hostname + ':8080' + req.url);
+})
+http.createServer(httpApp).listen(HTTP_PORT);
+
 
 const PORT = 8080;
 
 // GET home route
 app.get('/', (req, res) => {
 		res.writeHead(200);
-        res.end('Sent from Clipboard API v1');
+        res.end('Sent from Clipboard API v1 - ' + new Date());
 });
 
 const options = {
