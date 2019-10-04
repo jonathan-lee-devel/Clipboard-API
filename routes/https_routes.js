7cd5a71dc8f6c45a83a40872c7a4641dd50d3e62
Jonathan Lee <jonathan.lee.devel@gmail.com>
2019-10-05 00:07:14 +0100

    Added index response for HTTP

--- routes/https_routes.js
+++ routes/https_routes.js
@@ -3,6 +3,11 @@
 module.exports = function(app) {
   var customerController = require('../controllers/customerController');
 
+  app.route('/')
+  .get( (req, res) => {
+    res.send('Sent from Clipboard API v1 - ' + new Date());
+  });
+
   // Customer Routes
   app.route('/customers')
     .get(customerController.list_all_customers)
