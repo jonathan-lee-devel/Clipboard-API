863904f68fdf93598a86623a33077fe7fc02fc0a
Jonathan Lee <jonathan.lee.devel@gmail.com>
2019-10-05 00:51:50 +0100

    Reformatted/tidied code in customer controller and model

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -2,64 +2,68 @@
 
 var Customer = require('../models/customerModel');
 
-exports.list_all_customers = function(req, res) {
-    Customer.getAllCustomers(function(err, customer) {
+exports.list_all_customers = function (req, res) {
+  Customer.getAllCustomers( function (err, customer) {
 
-    console.log('controller')
-    if (err)
+    console.log('Controller');
+    if (err) {
       res.send(err);
+    }
+    else {
       console.log('res', customer);
-    res.send(customer);
+      res.send(customer);
+    }
   });
 };
 
-
-
-exports.create_a_customer = function(req, res) {
+exports.create_a_customer = function (req, res) {
   var new_customer = new Customer(req.body);
 
-  //handles null error 
-   if(!new_customer.name || !new_customer.address){
-
-            res.status(400).send({ error:true, message: 'Please provide name/address' });
-
-        }
-else{
-  
-  Customer.createCustomer(new_customer, function(err, customer) {
-    
-    if (err)
-      res.send(err);
-    res.json(customer);
-  });
-}
+  // Handles null error
+  if (!new_customer.name || !new_customer.address) {
+    res.status(400).send({ error: true, message: 'Must provide cusomer name & address' });
+  }
+  else {
+    Customer.createCustomer(new_customer, function (err, customer) {
+      if (err) {
+        res.send(err);
+      }
+      else {
+        res.json(customer);
+      }
+    });
+  }
 };
 
-
-exports.read_a_customer = function(req, res) {
-  Customer.getCustomerById(req.params.customerId, function(err, customer) {
-    if (err)
+exports.read_a_customer = function (req, res) {
+  Customer.getCustomerById(req.params.customerId, function (err, customer) {
+    if (err) {
       res.send(err);
-    res.json(customer);
-  });
-};
-
-
-exports.update_a_customer = function(req, res) {
-  Customer.updateById(req.params.customerId, new Customer(req.body), function(err, customer) {
-    if (err)
-      res.send(err);
-    res.json(customer);
+    }
+    else {
+      res.json(customer);
+    }
   });
 };
 
-
-exports.delete_a_customer = function(req, res) {
-
-
-  Customer.remove( req.params.customerId, function(err, customer) {
-    if (err)
+exports.update_a_customer = function (req, res) {
+  Customer.updateCustomerById(req.params.customerId, new Customer(req.body), function (err, customer) {
+    if (err) {
       res.send(err);
-    res.json({ message: 'Customer successfully deleted' });
+    }
+    else {
+      res.json(customer);
+    }
+  });
+};
+
+exports.delete_a_customer = function (req, res) {
+  Customer.remove(req.params.customerId, function (err, customer) {
+    if (err) {
+      res.send(err);
+    }
+    else {
+      res.json({ message: 'Customer successfully deleted' });
+    }
   });
 };
--- models/customerModel.js
+++ models/customerModel.js
@@ -1,74 +1,69 @@
 'user strict';
-var sql = require('../util/mysql-connection');
+const sql = require('../util/mysql-connection');
 
-//Task object constructor
-var Customer = function(customer){
+// Customer object constructor
+const Customer = function(customer){
     this.name = customer.name;
     this.address = customer.address;
     this.created_at = new Date();
 };
-Customer.createCustomer = function (newCustomer, result) {    
-        sql.query("INSERT INTO customers set ?", newCustomer, function (err, res) {
-                
-                if(err) {
-                    console.log("error: ", err);
-                    result(err, null);
-                }
-                else{
-                    console.log(res.insertId);
-                    result(null, res.insertId);
-                }
-            });           
+
+Customer.createCustomer = function (customer, result) {
+    sql.query("INSERT INTO customers set ?", customer, function (err, res) {
+        if (err) {
+            console.log("Error: ", err);
+            result(err, null);
+        }
+    })
 };
-Customer.getCustomerById = function (customerId, result) {
-        sql.query("SELECT * FROM customers WHERE id = ? ", customerId, function (err, res) {             
-                if(err) {
-                    console.log("error: ", err);
-                    result(err, null);
-                }
-                else{
-                    result(null, res);
-              
-                }
-            });   
+
+Customer.getCustomerById = function (id, result) {
+    sql.query("SELECT * FROM customers WHERE id = ? ", id, function (err, res) {
+        if (err) {
+            console.log("Error: ", err);
+            result(err, null);
+        }
+        else {
+            result(null, res);
+        }
+    });
 };
+
 Customer.getAllCustomers = function (result) {
-        sql.query("SELECT * FROM customers", function (err, res) {
-
-                if(err) {
-                    console.log("error: ", err);
-                    result(null, err);
-                }
-                else{
-                  console.log('tasks : ', res);  
-
-                 result(null, res);
-                }
-            });   
+    sql.query("SELECT * FROM customers", function (err, res) {
+        if (err) {
+            console.log("Error: ", err);
+            result(null, err);
+        }
+        else {
+            console.log("Customers: ", res);
+            result(null, res);
+        }
+    });
 };
-Customer.updateById = function(id, task, result){
-  sql.query("UPDATE customers SET customer = ? WHERE id = ?", [task.task, id], function (err, res) {
-          if(err) {
-              console.log("error: ", err);
-                result(null, err);
-             }
-           else{   
-             result(null, res);
-                }
-            }); 
-};
-Customer.remove = function(id, result){
-     sql.query("DELETE FROM customers WHERE id = ?", [id], function (err, res) {
 
-                if(err) {
-                    console.log("error: ", err);
-                    result(null, err);
-                }
-                else{
-               
-                 result(null, res);
-                }
-            }); 
+Customer.updateCustomerById = function(id, customer, result) {
+    sql.query("UPDATE customers SET customer = ? WHERE id = ?", [customer.customer, id], function (err, res) {
+        if (err) {
+            console.log("Error: ", err);
+            result(null, err);
+        }
+        else {
+            result(null, res);
+        }
+    });
 };
+
+Customer.removeCustomerById = function(id, result) {
+    sql.query("DELETE FROM customers WHERE id = ?", [id], function (err, res) {
+        if (err) {
+            console.log("Error: ", err);
+            result(null, err);
+        }
+        else {
+            result(null, res);
+        }
+    })
+}
 
 module.exports= Customer;
