b81f46b4bf44046e8f7f82979e470a115666d688
Jonathan Lee <jonathan.lee.devel@gmail.com>
2019-10-06 01:08:48 +0100

    Added unfinished loginVerification

--- controllers/userController.js
+++ controllers/userController.js
@@ -66,33 +66,47 @@ exports.find_a_user_by_email = (req, res) => {
   }
 };
 
+async function getUserProvidedPartialLogin(user) {
+  if (!user.email) {// If e-mail address is not given, search by username
+    User.findUserByUsername(user.username, (err, result) => {
+      if (err) throw err;
+      console.log('@ASYNC: ', result);
+    })
+    .then( result => {
+      return result;
+    }).catch( err => console.log(err) );
+  } else {// Otherwise search by e-mail address
+    User.findUserByEmail(user.email, (err, result) => {
+      if (err) throw err;
+      console.log('@ASYNC_EMAIL: ', result);
+    })
+    .then( result => {
+      return result;
+    }).catch( err => console.log(err) );
+  }
+}
+
 exports.verify_user = (req, res) => {
-  var check_user = new User(req.body);
+  const check_user = new User(req.body);
+  let hash = null;
 
   // Handles null error
   if ( (!check_user.username && !check_user.email) || !check_user.password ) {
     res.status(400).send({ error: true, message: 'Invalid user' });
   }
   else {
-    // Hash password
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(check_user.password, salt, (err, hash) => {
-        if (err) {
-          throw err;
-        }
-        else {
-          // Set password to hash result before passing to model
-          check_user.password = hash;
-          User.verifyUser(check_user, (err, user) => {
-            if (err) {
-              res.send(err);
-            }
-            else {
-              res.json(user);
-            }
-          });
-        }
-      });
-    });
+    getUserProvidedPartialLogin(check_user)
+    .then( (result) => {
+      hash = result[0].password;
+      console.log('@verify_user: ', hash);
+      console.log('@compare: ', check_user.password, hash);
+      bcrypt.compare(check_user.password, hash)
+      .then(result => {
+        console.log(res);
+        res.send(result);
+      })
+      .catch(err => console.error(err.message));
+
+    }).catch( err => console.log(err) );
   }
 };
