f31bf87a48deaafa4973b1749a02e6abf147924d
Jonathan Lee <jonathan.lee.devel@gmail.com>
2019-12-30 01:03:18 +0000

    Added basic db config

---
+++ config/db/db.config.js
@@ -0,0 +1,9 @@
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    HOST: process.env.DB_HOST,
+    database: process.env.DB_DB,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+});
