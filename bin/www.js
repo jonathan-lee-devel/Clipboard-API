36aac8481ef60558ee0065bd97cd8c5f5dd60b5c
Jonathan Lee <jonathan.lee.devel@gmail.com>
2020-01-01 05:07:41 +0000

    Documentation & code formatting

--- bin/www.js
+++ bin/www.js
@@ -1,45 +1,39 @@
 #!/usr/bin/env node
 
+/* IMPORTS */
 const https_app = require("../applications/https_app");
 const http_redirect_app = require("../applications/http_redirect_app");
-
 const debug = require("debug")("test:server");
 const https = require("https");
 const http = require("http");
 
-/**
- * Get port from environment and store in Express.
- */
-const https_port = normalizePort(process.env.HTTPS_PORT || "8080");
-const http_redirect_port = normalizePort(process.env.HTTP_PORT || "3000");
-https_app.set("port", https_port);
-http_redirect_app.set("port", http_redirect_port);
+/* CREATE & CONFIGURE HTTPS SERVER/APPLICATION */
 
-/**
- * Create HTTPS Server
- */
+// Configure application port
+const https_port = normalizePort(process.env.HTTPS_PORT || "8080");
+https_app.set("port", https_port);
+
+// Configure SSL
 const https_options = {
-  key: process.env.HTTPS_SSL_KEY, 
+  key: process.env.HTTPS_SSL_KEY,
   cert: process.env.HTTPS_SSL_CERT,
   passphrase: process.env.HTTPS_SSL_PASSPHRASE
 };
+
+// Create server
 const https_server = https.createServer(https_options, https_app);
-
-/**
- * Create HTTP Redirect Server
- */
-const http_redirect_server = http.createServer(http_redirect_app);
-
-/**
- * Configure HTTPS Server to Listen on HTTPS PORT
- */
 https_server.listen(https_port);
 https_server.on("error", https_onError);
 https_server.on("listening", https_onListening);
 
-/**
- * Configure HTTP Redirect Server to Listen on HTTP PORT
- */
+/* CREATE & CONFIGURE HTTP REDIRECT SERVER/APPLICATION */
+
+// Confiugre application port
+const http_redirect_port = normalizePort(process.env.HTTP_PORT || "3000");
+http_redirect_app.set("port", http_redirect_port);
+
+// Create server
+const http_redirect_server = http.createServer(http_redirect_app);
 http_redirect_server.listen(http_redirect_port);
 http_redirect_server.on("error", http_redirect_onError);
 http_redirect_server.on("listening", http_redirect_onListening);
@@ -50,11 +44,13 @@ http_redirect_server.on("listening", http_redirect_onListening);
 function normalizePort(val) {
   var port = parseInt(val, 10);
 
-  if (isNaN(port)) {// Named pipe
+  if (isNaN(port)) {
+    // Named pipe
     return val;
   }
 
-  if (port >= 0) {// Port number
+  if (port >= 0) {
+    // Port number
     return port;
   }
 
@@ -64,7 +60,6 @@ function normalizePort(val) {
 /**
  * Event listener for HTTPS server "error" event.
  */
-
 function https_onError(error) {
   if (error.syscall !== "listen") {
     throw error;
@@ -93,7 +88,6 @@ function https_onError(error) {
 /**
  * Event listener for HTTPS server "listening" event.
  */
-
 function https_onListening() {
   var addr = https_server.address();
   var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
@@ -103,14 +97,15 @@ function https_onListening() {
 /**
  * Event listener for HTTP server "error" event.
  */
-
 function http_redirect_onError(error) {
   if (error.syscall !== "listen") {
     throw error;
   }
 
   var bind =
-    typeof http_redirect_port === "string" ? "Pipe " + http_redirect_port : "Port " + http_redirect_port;
+    typeof http_redirect_port === "string"
+      ? "Pipe " + http_redirect_port
+      : "Port " + http_redirect_port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
@@ -130,7 +125,6 @@ function http_redirect_onError(error) {
 /**
  * Event listener for HTTP server "listening" event.
  */
-
 function http_redirect_onListening() {
   var addr = http_redirect_server.address();
   var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
