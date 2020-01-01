0b71c518d1cb28ff9dd268a98104f43e3456cb68
Jonathan Lee <jonathan.lee.devel@gmail.com>
2020-01-01 05:17:19 +0000

    Removed unused code

--- routers/https/https_customer_router.js
+++ routers/https/https_customer_router.js
@@ -2,14 +2,4 @@ const express = require('express');
 const router = express.Router();
 const customer_controller = require('../../controllers/customer_controller');
 
-router.get('/', customer_controller.list_all_customers);
-
-router.post('/create', customer_controller.create_customer);
-
-router.get('find/:customer_id', customer_controller.find_customer_by_id);
-
-router.put('update/:customer_id', customer_controller.update_customer);
-
-router.delete('delete/:customer_id', customer_controller.delete_customer);
-
 module.exports = router;
--- routers/https/https_user_router.js
+++ routers/https/https_user_router.js
@@ -2,35 +2,6 @@ const express = require("express");
 const router = express.Router();
 const user_controller = require("../../controllers/user_controller");
 
-function check_authenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-
-  res.redirect("/login");
-}
-
-function check_not_authenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return res.redirect("/");
-  }
-
-  next();
-}
-
-// Will require POST conversion to DELETE,
-// DELETE always safer when deleting
-router.delete('/logout', (req, res) => {
-    req.logOut();
-    req.redirect('/login');
-})
-
-router.get("/", check_authenticated, user_controller.list_all_users);
-
-router.get("/find/:user_email", user_controller.find_user_by_email);
-
-router.post("/register", user_controller.register_user);
-
-router.post("/login", user_controller.verify_user);
+router.get('/', user_controller.)
 
 module.exports = router;
